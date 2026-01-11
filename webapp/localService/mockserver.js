31893a64e59a2919699120588fbcb9ad05a5a22c
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2026-01-11 16:35:47 +0100

    Refactor mock server to handle FindUpcomingMeetups and update list binding in App.view.xml

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,7 +1,9 @@
 sap.ui.define([
+	"sap/ui/thirdparty/jquery",
+	"sap/ui/core/date/UI5Date",
 	"sap/ui/core/util/MockServer",
 	"sap/base/Log"
-], (MockServer, Log) => {
+], (jQuery, UI5Date, MockServer, Log) => {
 	"use strict";
 
 	return {
@@ -15,20 +17,45 @@ sap.ui.define([
 			// create
 			const oMockServer = new MockServer({ rootUri: "/" });
 
-			// simulate against the metadata and mock data
 			oMockServer.simulate("../localService/metadata.xml", {
 				sMockdataBaseUrl: "../localService/mockdata",
 				bGenerateMissingMockData: true
 			});
 
-            // handling custom URL parameter step
-            const fnCustom = (oEvent) => {
-                const oXhr = oEvent.getParameter("oXhr");
-                if (oXhr?.url.includes("first")) {
-                    oEvent.getParameter("oFilteredData").results.splice(3, 100);
-                }
-            };
-            oMockServer.attachAfter("GET", fnCustom, "Meetups");
+			// handling mocking a function import call step
+			const aRequests = oMockServer.getRequests();
+			aRequests.push({
+				method: "GET",
+				path: new RegExp("FindUpcomingMeetups(.*)"),
+				response: (oXhr) => {
+					Log.debug("Incoming request for FindUpcomingMeetups");
+					const oToday = UI5Date.getInstance();
+					oToday.setHours(0); // or today.toUTCString(0) due to timezone differences
+					oToday.setMinutes(0);
+					oToday.setSeconds(0);
+					// the mock server only works with sap.jQuery.ajax and async: false. But the request does not
+					// actually go to a server, so it does not block the main thread.
+					jQuery.ajax({
+						url: `/Meetups?$filter=EventDate ge datetime'${oToday.toISOString()}'`,
+						dataType : 'json',
+						async: false,
+						success : (oData) => {
+							oXhr.respondJSON(200, {}, JSON.stringify(oData));
+						}
+					});
+					return true;
+				}
+			});
+			oMockServer.setRequests(aRequests);
+
+			// handling custom URL parameter step
+			const fnCustom = (oEvent) => {
+				const oXhr = oEvent.getParameter("oXhr");
+				if (oXhr?.url.includes("first")) {
+					oEvent.getParameter("oFilteredData").results.splice(3, 100);
+				}
+			};
+			oMockServer.attachAfter("GET", fnCustom, "Meetups");
 
 			// start
 			oMockServer.start();
